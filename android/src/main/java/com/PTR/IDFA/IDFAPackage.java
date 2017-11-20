b9141231dd0f1536d0836fb34e9f41825061cc15
Писарев Кирилл <k.pisarev@maximatelecom.ru>
2017-11-20 16:12:26 +0300

    Add rn < 47 support

--- android/src/main/java/com/PTR/IDFA/IDFAPackage.java
+++ android/src/main/java/com/PTR/IDFA/IDFAPackage.java
@@ -19,6 +19,11 @@ public class IDFAPackage implements ReactPackage {
         );
     }
 
+    // support rn < 0.47
+    public List<Class<? extends JavaScriptModule>> createJSModules() {
+        return Collections.emptyList();
+    }
+
     @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
         return Collections.emptyList();
